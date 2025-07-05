98510953c80b974a0109ccd72b19224d4553f38c
Aurimas <kacasaurimas@gmail.com>
2025-07-05 19:24:23 +0300

    should work for initial routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import viteLogo from '/vite.svg'
+import { Suspense } from 'react'
+import { Outlet } from 'react-router-dom'
+import { Header } from './components/Header'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-
-        <a
-          href="https://vite.dev"
-          target="_blank"
-        >
-          <img
-            alt="Vite logo"
-            className="logo"
-            src={viteLogo}
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-        >
-          <img
-            alt="React logo"
-            className="logo react"
-            src={reactLogo}
-          />
-        </a>
+      <Header />
+      <div className="container">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          type="button"
-          onClick={() => setCount(count => count + 1)}
-        >
-          count is
-          {' '}
-          {count}
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <p>Browse movies or characters</p>
+    </main>
+  )
+}
